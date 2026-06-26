c1d31857ca28eb21dd453f93744d6d57df308d5d
Fred819uk <215203661+Fred819uk@users.noreply.github.com>
2026-06-26 11:50:22 +0100

    test

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -4,6 +4,7 @@ import { registerStart } from "./Yuu API/RegisterStart";
 import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
 import { Quaternion } from "./Yuu API/Basic Types/Quaternion";
 import { Color } from "./Yuu API/Basic Types/Color";
+import { Events } from "./Yuu API/Events";
 
 
 registerStart(start);
@@ -12,7 +13,22 @@ function start() {
 
   console.log('Testing 1,2,3!');
 
-  // spawn a white sphere at (0,1,0)
-  spawnPrimitive.sphere(16, 16, new Vector3(5, 1, 0), 1, Quaternion.one, Color.red, 1, 'Sphere', 'Static', undefined);
+  // spawn spheres
+  const sphere1 = spawnPrimitive.sphere(16, 16, new Vector3(5, 1, 0), 1, Quaternion.one, Color.red, 1, 'Sphere', 'Static', undefined);
   spawnPrimitive.sphere(16, 16, new Vector3(0, 1, 5), 1, Quaternion.one, Color.blue, 1, 'Sphere', 'Static', undefined);
+
+  // animate first sphere: move up/down by 5 units peak-to-peak over 5 seconds (±2.5 amplitude)
+  let t = 0;
+  const period = 5; // seconds for a full up/down cycle
+  const amplitude = 2.5; // half of peak-to-peak distance (5/2)
+
+  Events.onUpdate((deltaTime: number) => {
+    t += deltaTime;
+
+    if (sphere1 && sphere1.exists()) {
+      const offset = Math.sin((t / period) * Math.PI * 2) * amplitude;
+      const basePos = new Vector3(5, 1, 0);
+      sphere1.pos = new Vector3(basePos.x, basePos.y + offset, basePos.z);
+    }
+  });
 }
