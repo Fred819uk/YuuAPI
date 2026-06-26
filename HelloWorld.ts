86c41793a8508f8680291ad0efdd51b1984929e7
Fred819uk <215203661+Fred819uk@users.noreply.github.com>
2026-06-26 11:43:18 +0100

    test

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,6 +1,9 @@
 import { Vector3 } from "./Yuu API/Basic Types/Vector3";
 import { inWorldConsole } from "./Yuu API/Console";
 import { registerStart } from "./Yuu API/RegisterStart";
+import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
+import { Quaternion } from "./Yuu API/Basic Types/Quaternion";
+import { Color } from "./Yuu API/Basic Types/Color";
 
 
 registerStart(start);
@@ -8,4 +11,7 @@ function start() {
   inWorldConsole.visible(true, new Vector3(0, 1.5, -1.5));
 
   console.log('Testing 1,2,3!');
+
+  // spawn a white sphere at (0,1,0)
+  spawnPrimitive.sphere(16, 16, new Vector3(0, 1, 0), 1, Quaternion.one, Color.white, 1, 'Sphere', 'Static', undefined);
 }
